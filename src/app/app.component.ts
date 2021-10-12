90d59593c9b5fdc438dc3045d6c79eeade8bd1c6
jordicuevas <jordicuevas@gmail.com>
2021-10-12 16:40:21 -0400

    map, take and tap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, VERSION } from '@angular/core';
 import { of, from } from 'rxjs';
+import { map, tap, take } from 'rxjs/operators';
+
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
@@ -12,17 +14,31 @@ export class AppComponent implements OnInit {
     error: (err: Error) => console.log(`The error is ${err}`),
     complete: () => console.log(`Stream completed`),
   };
-
+  observerNumbers = {
+    next: (item) => console.log(`The number is ${item}`),
+    error: (err) => console.log(`The error is ${err}`),
+    complete: () => console.log(`Stream completed`),
+  };
   ngOnInit(): void {
     of(2, 4, 6, 8).subscribe(console.log);
 
-    from([20, 15, 10, 5]).subscribe(
-      (item) => console.log(`The number is ${item}`),
-      (err) => console.log(`The error is ${err}`),
-      () => console.log(`Stream completed`)
-    );
+    from([20, 15, 10, 5])
+      .pipe(
+        tap((item) => console.log(`emitted item ${item}`)),
+        map((item) => item * 2),
+        map((item) => item - 10),
+        map((item) => {
+          if (item === 0) {
+            throw new Error('zero detected');
+          }
+          return item;
+        }),
+        take(3)
+      )
+      .subscribe(this.observerNumbers);
 
     of('Apple1', 'Apple2', 'Apple3').subscribe(this.observerApples);
+    // operator
   }
 }
 
